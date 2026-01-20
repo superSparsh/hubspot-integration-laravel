a39075ec1cea61e1ccb9230dcd1677d0127f4b00
Sparsh <sparsh@hubspot-integration.local>
2026-01-20 09:21:44 +0530

    Hubspot Scopes

--- config/hubspot.php
+++ config/hubspot.php
@@ -10,7 +10,7 @@ return [
     'client_id' => env('HUBSPOT_CLIENT_ID'),
     'client_secret' => env('HUBSPOT_CLIENT_SECRET'),
     'redirect_uri' => env('HUBSPOT_REDIRECT_URI', 'https://hubspot.tittu.in/integrations/hubspot/callback'),
-    'scopes' => env('HUBSPOT_SCOPES', 'oauth,crm.objects.contacts.read,crm.objects.deals.read'),
+    'scopes' => env('HUBSPOT_SCOPES', 'crm.objects.contacts.read crm.objects.deals.read'),
     'api_base' => env('HUBSPOT_API_BASE', 'https://api.hubapi.com'),
     'webhook_secret' => env('HUBSPOT_WEBHOOK_SECRET'),
 ];
